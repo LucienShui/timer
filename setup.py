c8168d439e662773e1a306bf9b0564b0f92b6939
Lucien Shui <lucien@lucien.ink>
2021-03-16 11:05:28 +0800

    Bump version, add __version__.py, update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, print_function
 # read the contents of your README file
 from os import path
 
+from timer.__version__ import __name__, __version__, __author__, __author_email__, __url__, __description__
+
 from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -10,12 +12,12 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
-    name='timer',
-    version='0.1.0',
-    author='Lucien Shui',
-    author_email='lucien@lucien.ink',
-    url='https://github.com/LucienShui/timer',
-    description=u'Python Code Timer',
+    name=__name__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    description=__description__,
     packages=['timer'],
     long_description=long_description,
     long_description_content_type='text/markdown'
---
+++ timer/__version__.py
@@ -0,0 +1,10 @@
+__name__ = 'timer'
+__description__ = 'Python Code Timer'
+__url__ = 'https://github.com/LucienShui/timer'
+__version_info__ = (0, 1, 1)
+__version__ = '.'.join(map(str, __version_info__))
+__build__ = eval(f"0x{''.join(map(lambda x: f'{int(x):02d}', __version_info__))}")
+__author__ = 'Lucien Shui'
+__author_email__ = 'lucien@lucien.ink'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2021 Lucien Shui'
