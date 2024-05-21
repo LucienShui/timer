7b801a9829948568251eaec7300ba7972cbf16c1
Lucien Shui <lucien@lucien.ink>
2024-05-21 12:05:49 +0800

    Bump version to 0.3.0

--- timer/__version__.py
+++ timer/__version__.py
@@ -1,7 +1,7 @@
 __name__ = 'timer'
 __description__ = 'Python Code Timer'
 __url__ = 'https://github.com/LucienShui/timer'
-__version_info__ = (0, 2, 3)
+__version_info__ = (0, 3, 0)
 __version__ = '.'.join(map(str, __version_info__))
 __build__ = eval(f"0x{''.join(map(lambda x: f'{int(x):02d}', __version_info__))}")
 __author__ = 'Lucien Shui'
